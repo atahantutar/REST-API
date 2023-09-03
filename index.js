ba9bfcd2234242baa2f7f4dcfa5369e8e7d95674
Atahan <atahantutar69@gmail.com>
2023-09-04 02:47:57 +0300

    feat: cors settings on server

--- index.js
+++ index.js
@@ -3,13 +3,16 @@ const cors = require("cors");
 const dotenv = require("dotenv");
 const app = express();
 const db = require("./config/database");
+const Auth = require("./routes/auth.js");
 dotenv.config();
 
 app.use(cors());
-app.use(express.json());
+app.use(express.json({ limit: "30mb", extended: true }));
+app.use(express.urlencoded({ limit: "30mb", extended: true }));
 
-app.get("/", (req, res) => {
-  res.status(200).json({ messaage: "asdad" });
+app.use("/", Auth);
+app.set("/", (req, res) => {
+  res.json({ message: "deneme" });
 });
 
 db();
