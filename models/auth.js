6c701f95df345247e62faa73343d5fdb4465fae9
Atahan <atahantutar69@gmail.com>
2023-09-04 02:43:25 +0300

    feat: added auth model

--- models/auth.js
+++ models/auth.js
@@ -1,1 +1,10 @@
 const mongoose = require("mongoose");
+
+const AuthSchema = new mongoose.Schema({
+  username: { type: String, require: true, trim: true },
+  email: { type: String, require: true, unique: true },
+  password: { type: String, require: true },
+  date: { type: Date, default: new Date() },
+});
+
+module.exports = mongoose.model("auth", AuthSchema);
