1140f33e68f0e9bbc7caa36eef230ebe9e57b413
Atahan <atahantutar69@gmail.com>
2023-09-06 23:21:33 +0300

    refactoring

--- routes/post.js
+++ routes/post.js
@@ -1,11 +1,17 @@
 const express = require("express");
 const router = express.Router();
-const {} = require("../controllers/post.js");
+const {
+  getPosts,
+  createPost,
+  getDetail,
+  deletePost,
+  getUpdate,
+} = require("../controllers/post.js");
 
-router.get("/createPost", createPost);
-router.get("getUpdate/:id", getUpdate);
-router.delete("deletePost/:id", deletePost);
-router.get("getDetail/:id", getDetail);
 router.get("/getPosts", getPosts);
+router.post("/createPost", createPost);
+router.get("/getDetail/:id", getDetail);
+router.delete("/deletePost/:id", deletePost);
+router.get("/getUpdate/:id", getUpdate);
 
 module.exports = router;
